b9d8389389189bd35d33ddf0ebe9d5422988f259
CreativeMD <Creative-Multimedia-Design@gmx.de>
2016-07-29 01:13:56 +0200

    Enhanced GUI loading

--- src/main/java/com/creativemd/opf/gui/SubContainerPic.java
+++ src/main/java/com/creativemd/opf/gui/SubContainerPic.java
@@ -1,24 +1,32 @@
 package com.creativemd.opf.gui;
 
 import com.creativemd.creativecore.gui.container.SubContainer;
+import com.creativemd.creativecore.gui.premade.SubContainerTileEntity;
 import com.creativemd.opf.block.TileEntityPicFrame;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 
-public class SubContainerPic extends SubContainer {
+public class SubContainerPic extends SubContainerTileEntity {
 	
 	public TileEntityPicFrame frame;
 	
 	public SubContainerPic(TileEntityPicFrame frame, EntityPlayer player) {
-		super(player);
+		super(player, frame);
 		this.frame = frame;
+	}
+	
+	@Override
+	public boolean shouldTick()
+	{
+		return false;
 	}
 
 	@Override
 	public void createControls() {
 		
 	}
+	
 	@Override
 	public void onPacketReceive(NBTTagCompound nbt) {
 		if(nbt.getInteger("type") == 0)
