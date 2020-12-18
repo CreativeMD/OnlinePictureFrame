d7b1000080544b661caa8ec6d42b81644b3e98a9
CreativeMD <Creative-Multimedia-Design@gmx.de>
2020-12-18 14:04:55 +0100

    Reverted back to single process players

--- src/main/java/com/creativemd/opf/client/VideoTexture.java
+++ src/main/java/com/creativemd/opf/client/VideoTexture.java
@@ -1,32 +1,23 @@
 package com.creativemd.opf.client;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.lwjgl.opengl.GL11;
 
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraftforge.fml.common.Loader;
-import team.creative.onlinepictureframe.lib.network.InterProcessError;
-import team.creative.onlinepictureframe.lib.network.InterProcessNetwork;
-import team.creative.onlinepictureframe.lib.network.InterProcessPacket;
-import team.creative.onlinepictureframe.lib.network.packet.BufferPacket;
-import team.creative.onlinepictureframe.lib.network.packet.DimensionPacket;
-import team.creative.onlinepictureframe.lib.network.packet.ErrorPacket;
-import team.creative.onlinepictureframe.lib.network.packet.UpdatePacket;
-import team.creative.onlinepictureframe.lib.network.packet.VolumePacket;
+import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.base.MediaPlayer;
+import uk.co.caprica.vlcj.player.component.CallbackMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.embedded.videosurface.callback.BufferFormat;
+import uk.co.caprica.vlcj.player.embedded.videosurface.callback.BufferFormatCallback;
+import uk.co.caprica.vlcj.player.embedded.videosurface.callback.RenderCallback;
 
 public class VideoTexture extends PictureTexture {
 	
-	static final File vlcj = new File(new File(new File(Loader.instance().getConfigDir().getParent(), "mods"), "1.12.2"), "OnlinePictureFrameVLCJ_v1.0.1.jar");
-	
+	public CallbackMediaPlayerComponent player;
 	public ByteBuffer buffer;
 	public int texture;
-	private Process process;
-	private InterProcessNetwork network;
 	private AtomicBoolean needsUpdate = new AtomicBoolean(false);
 	private boolean first = true;
 	
@@ -34,52 +25,32 @@ public class VideoTexture extends PictureTexture {
 		super(100, 100);
 		texture = GlStateManager.generateTexture();
 		
-		try {
-			process = Runtime.getRuntime().exec("java -jar \"" + vlcj + "\" \"" + url + "\"");
-			network = new InterProcessNetwork(process.getInputStream(), process.getOutputStream()) {
+		player = new CallbackMediaPlayerComponent(new MediaPlayerFactory("--quiet"), null, null, false, new RenderCallback() {
+			
+			@Override
+			public void display(MediaPlayer mediaPlayer, ByteBuffer[] nativeBuffers, BufferFormat bufferFormat) {
+				buffer = nativeBuffers[0];
+				needsUpdate.set(true);
+			}
+		}, new BufferFormatCallback() {
+			
+			@Override
+			public BufferFormat getBufferFormat(int sourceWidth, int sourceHeight) {
+				VideoTexture.this.width = sourceWidth;
+				VideoTexture.this.height = sourceHeight;
+				VideoTexture.this.first = true;
+				return new BufferFormat("RGBA", sourceWidth, sourceHeight, new int[] { sourceWidth * 4 }, new int[] { sourceHeight });
+			}
+			
+			@Override
+			public void allocatedBuffers(ByteBuffer[] buffers) {
 				
-				@Override
-				public void process(InterProcessPacket packet) {
-					if (packet instanceof ErrorPacket) {
-						if (((ErrorPacket) packet).error == InterProcessError.FAILED_LOAD_VIDEO)
-							network.release();
-					} else if (packet instanceof UpdatePacket)
-						needsUpdate.set(true);
-					else if (packet instanceof DimensionPacket) {
-						synchronized (VideoTexture.this) {
-							width = ((DimensionPacket) packet).width;
-							height = ((DimensionPacket) packet).height;
-							first = true;
-						}
-					} else if (packet instanceof BufferPacket) {
-						synchronized (VideoTexture.this) {
-							buffer = ((BufferPacket) packet).buffer;
-						}
-						/*System.out.println("newbuffer:" + ((BufferPacket) packet).address + "," + ((BufferPacket) packet).capacity);
-						//buffer = new Pointer(((BufferPacket) packet).address).getByteBuffer(0, ((BufferPacket) packet).capacity);
-						buffer = UnsafeByteBufferUtils.create(((BufferPacket) packet).address, ((BufferPacket) packet).capacity);
-						System.out.println("Updated buffer " + buffer);*/
-					}
-				}
-				
-				@Override
-				public boolean isMod() {
-					return true;
-				}
-				
-				@Override
-				public void receive(InterProcessPacket packet, InputStream in) throws IOException {
-					if (packet instanceof BufferPacket)
-						synchronized (VideoTexture.this) {
-							((BufferPacket) packet).buffer = buffer;
-							packet.receive(in);
-						}
-				}
-			};
-			network.send(new VolumePacket(0));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			}
+			
+		}, null);
+		player.mediaPlayer().submit(() -> {
+			player.mediaPlayer().media().start(url);
+		});
 	}
 	
 	@Override
@@ -111,16 +82,8 @@ public class VideoTexture extends PictureTexture {
 	
 	@Override
 	public void release() {
-		if (process.isAlive())
-			process.destroyForcibly();
+		player.release();
 		super.release();
-	}
-	
-	@Override
-	protected void finalize() throws Throwable {
-		if (process.isAlive())
-			process.destroyForcibly();
-		super.finalize();
 	}
 	
 	@Override
